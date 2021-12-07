2986fcd5466aef629216da6cc7bad52abe2c0f84
Seokjin Yoon <sjy5386@gmail.com>
2021-12-08 00:48:29 +0900

    Add open image

--- app/src/main/java/com/sysbot32/exifor/MainActivity.kt
+++ app/src/main/java/com/sysbot32/exifor/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.sysbot32.exifor
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.media.ExifInterface
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import com.sysbot32.exifor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.openButton.setOnClickListener {
+            startActivityForResult(
+                Intent().setAction(Intent.ACTION_PICK)
+                    .setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*"), 0
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                0 -> {
+                    val uri = data?.data!!
+                    binding.imageView.setImageURI(uri)
+                    val exif = ExifInterface(contentResolver.openInputStream(uri)!!)
+                    if (exif.hasAttribute(ExifInterface.TAG_DATETIME)) {
+                        binding.datetimeTextView.text =
+                            exif.getAttribute(ExifInterface.TAG_DATETIME)
+                    }
+                }
+            }
+        }
     }
 }
